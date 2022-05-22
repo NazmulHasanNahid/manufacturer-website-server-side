ee84cbda1a86e9e75c05dbdae0a7ee29f1243bfd
NazmulHasanNahid <nh.nahid4491@gmail.com>
2022-05-22 22:25:00 +0600

    mongodb connect

--- index.js
+++ index.js
@@ -2,10 +2,27 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 
 app.use(cors())
 app.use(express.json())
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wwafz.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run(){
+     try{
+          await client.connect()
+          const toolsCollection = client.db('manufacture').collection('tools')
+     }
+     finally{
+          //await client.close()
+     }
+}
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
   res.send('manufacturer server running')
